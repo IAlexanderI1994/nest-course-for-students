31cda1c1a39ee708889b5a9fdd7e203fd787241b
Alexander Kiryushin <kontaktAK@yandex.ru>
2020-06-19 00:00:53 +0300

    - adding postgres as docker service - config refactoring

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,8 +6,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: 'postgres',
-    database: 'taskmanagement',
-    entities: [__dirname + '/../**/*.entity.ts'],
+    password: 'postgres_password',
+    database: 'postgres',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 };
